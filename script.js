2c6bb1d4116a26df6446447f91bb62571d74a58f
poornima200631 <poornimatewatia48@gmail.com>
2025-03-19 22:40:51 +0530

    add some changes

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ function timetab(){
     const branch= document.getElementById("branch").value;
     const section= document.getElementById("section").value;
 
-    let filename="year" + "branch "+ "section"+ .html;
-    window.location.href = fileName;
+    let filename=year + branch + section+ ".html";
+    window.location.href =filename;
 }
