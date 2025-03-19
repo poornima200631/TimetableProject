803ecd92251a565c2c4656bb4e6002eb08c47642
poornima200631 <poornimatewatia48@gmail.com>
2025-03-19 19:49:02 +0530

    add some changes

---
+++ script.js
@@ -0,0 +1,8 @@
+function timetab(){
+    const year= document.getElementById("year").value;
+    const branch= document.getElementById("branch").value;
+    const section= document.getElementById("section").value;
+
+    let filename="year" + "branch "+ "section"+ .html;
+    window.location.href = fileName;
+}
